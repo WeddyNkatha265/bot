bcfb32ecfe62e5e6502ade6622bf54c683b20289
Weddy Nkatha <115021897+WeddyNkatha265@users.noreply.github.com>
2024-08-13 14:51:58 +0300

    Update YourBotArmy.js

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import BotCard from './BotCard';
 
-function YourBotArmy({ army, setArmy, deleteBot }) {
-  const releaseBot = (bot) => {
-    setArmy(army.filter(armyBot => armyBot.id !== bot.id));
-  };
-
+function YourBotArmy({ army, onRelease, onDischarge }) {
   return (
     <div className="your-bot-army">
       {army.map(bot => (
         <BotCard
           key={bot.id}
           bot={bot}
-          handleClick={() => releaseBot(bot)}
-          handleDelete={() => deleteBot(bot.id)}
+          onClick={() => onRelease(bot.id)}
+          onDischarge={() => onDischarge(bot.id)}
         />
       ))}
     </div>
