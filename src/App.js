be6244100b09707a6e3d6cb25095fb2a049fcbc0
Weddy Nkatha <115021897+WeddyNkatha265@users.noreply.github.com>
2024-08-13 14:52:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import BotCollection from './components/BotCollection';
 import YourBotArmy from './components/YourBotArmy';
+import './App.css';
 
 function App() {
-  const [bots, setBots] = useState([]);
   const [army, setArmy] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:8001/bots')
-      .then(response => response.json())
-      .then(data => setBots(data));
-  }, []);
+  const handleEnlist = (bot) => {
+    if (!army.some(item => item.id === bot.id)) {
+      setArmy([...army, bot]);
+    }
+  };
 
-  const deleteBot = (botId) => {
-    fetch(`http://localhost:8001/bots/${botId}`, {
-      method: 'DELETE'
-    })
-    .then(() => {
-      setArmy(army.filter(bot => bot.id !== botId));
-      setBots(bots.filter(bot => bot.id !== botId));
-    });
+  const handleRelease = (botId) => {
+    setArmy(army.filter(bot => bot.id !== botId));
+  };
+
+  const handleDischarge = (botId) => {
+    fetch(`http://localhost:8001/bots/${botId}`, { method: 'DELETE' })
+      .then(() => setArmy(army.filter(bot => bot.id !== botId)));
   };
 
   return (
     <div className="App">
-      <YourBotArmy army={army} setArmy={setArmy} deleteBot={deleteBot} />
-      <BotCollection bots={bots} setArmy={setArmy} army={army} deleteBot={deleteBot} />
+      <h1>Bot Battlr</h1>
+      <YourBotArmy army={army} onRelease={handleRelease} onDischarge={handleDischarge} />
+      <BotCollection onEnlist={handleEnlist} />
     </div>
   );
 }
